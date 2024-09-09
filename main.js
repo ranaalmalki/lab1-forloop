2830254e3747f0d636c55c61dbd8553c28869592
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-09 11:13:53 +0300

    solve the 3 q

---
+++ main.js
@@ -0,0 +1,24 @@
+for(let i =1 ; i >=-25 ; i--){
+    if(i%2==0)
+        console.log(i)
+}
+
+
+for(let i =1 ; i <=10 ; i++){
+    let num = i*i
+       console.log( num)
+}
+
+
+
+for(let i =1 ; i <=10 ; i++){
+    let num = i*i
+       console.log( num)
+}
+
+
+for(let i =1 ; i <=20 ; i++){
+    if(i%2!==0)
+        console.log(i)
+}
+
